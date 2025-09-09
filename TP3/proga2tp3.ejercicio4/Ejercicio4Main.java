877a132ba47d2f70df730910a87e00ba86e94d6e
ManuelNicanorWilliams <manu_williams@hotmail.com>
2025-09-09 15:11:59 -0300

    Create Ejercicio4Main.java

---
+++ TP3/proga2tp3.ejercicio4/Ejercicio4Main.java
@@ -0,0 +1,25 @@
+package proga2tp3.ejercicio4;
+
+
+public class Ejercicio4Main {
+    public static void main(String[] args) {
+        // Crear dos gallinas
+        Gallina gallina1 = new Gallina(1, 2);
+        Gallina gallina2 = new Gallina(2, 3);
+
+        // Simular acciones
+        gallina1.ponerHuevo();
+        gallina1.ponerHuevo();
+        gallina1.envejecer();
+
+        gallina2.ponerHuevo();
+        gallina2.envejecer();
+        gallina2.ponerHuevo();
+        gallina2.ponerHuevo();
+
+        // Mostrar estado final
+        System.out.println("\nEstado final de las gallinas:");
+        gallina1.mostrarEstado();
+        gallina2.mostrarEstado();
+    }
+}
