a1cfbe4b8abd73e89ae4dcc3bd98e71c38ba7d27
ManuelNicanorWilliams <manu_williams@hotmail.com>
2025-09-29 14:49:35 -0300

    Create Vehiculo.java

---
+++ TP5/ejercicio7/Vehiculo.java
@@ -0,0 +1,36 @@
+public class Vehiculo {
+    private String patente;
+    private String modelo;
+    private Motor motor;       // Agregación
+    private Conductor conductor; // Asociación bidireccional
+
+    public Vehiculo(String patente, String modelo, Motor motor, Conductor conductor) {
+        this.patente = patente;
+        this.modelo = modelo;
+        this.motor = motor;
+        this.conductor = conductor;
+        conductor.setVehiculo(this); // mantener relación bidireccional
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public Conductor getConductor() {
+        return conductor;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo [patente=" + patente + ", modelo=" + modelo + 
+               ", motor=" + motor + ", conductor=" + conductor + "]";
+    }
+}
