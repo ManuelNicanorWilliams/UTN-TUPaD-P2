7a207dbe83259fa1c4ea836595a100d373894ee2
ManuelNicanorWilliams <manu_williams@hotmail.com>
2025-09-29 14:34:29 -0300

    Create Usuario.java

---
+++ TP5/ejercicio2/Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+    private String nombre;
+    private String dni;
+    private Celular celular; // Asociación bidireccional
+
+    public Usuario(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public void setCelular(Celular celular) {
+        this.celular = celular;
+    }
+
+    public Celular getCelular() {
+        return celular;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [nombre=" + nombre + ", dni=" + dni + "]";
+    }
+}
