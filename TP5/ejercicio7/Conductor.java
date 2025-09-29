acff11ec8420dd427bb07d4ac023f9dd6594e66a
ManuelNicanorWilliams <manu_williams@hotmail.com>
2025-09-29 14:48:59 -0300

    Create Conductor.java

---
+++ TP5/ejercicio7/Conductor.java
@@ -0,0 +1,31 @@
+public class Conductor {
+    private String nombre;
+    private String licencia;
+    private Vehiculo vehiculo; // Asociación bidireccional
+
+    public Conductor(String nombre, String licencia) {
+        this.nombre = nombre;
+        this.licencia = licencia;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Conductor [nombre=" + nombre + ", licencia=" + licencia + "]";
+    }
+}
