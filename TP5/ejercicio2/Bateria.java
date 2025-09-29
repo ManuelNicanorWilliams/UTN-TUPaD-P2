b61384b898f601ff16bcf98d2f376abd3fa97ecb
ManuelNicanorWilliams <manu_williams@hotmail.com>
2025-09-29 14:33:24 -0300

    Create Bateria.java

---
+++ TP5/ejercicio2/Bateria.java
@@ -0,0 +1,22 @@
+public class Bateria {
+    private String modelo;
+    private int capacidad; // en mAh
+
+    public Bateria(String modelo, int capacidad) {
+        this.modelo = modelo;
+        this.capacidad = capacidad;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Bateria [modelo=" + modelo + ", capacidad=" + capacidad + "mAh]";
+    }
+}
