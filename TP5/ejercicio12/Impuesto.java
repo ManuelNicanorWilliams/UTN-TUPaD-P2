634f6fd51d9f666c589b05e5866ce6f8d7d07051
ManuelNicanorWilliams <manu_williams@hotmail.com>
2025-09-29 15:03:23 -0300

    Create Impuesto.java

---
+++ TP5/ejercicio12/Impuesto.java
@@ -0,0 +1,21 @@
+private double monto;
+    private Contribuyente contribuyente; // Asociación unidireccional
+
+    public Impuesto(double monto, Contribuyente contribuyente) {
+        this.monto = monto;
+        this.contribuyente = contribuyente;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public Contribuyente getContribuyente() {
+        return contribuyente;
+    }
+
+    @Override
+    public String toString() {
+        return "Impuesto [monto=" + monto + ", contribuyente=" + contribuyente + "]";
+    }
+}
