d45ac494117e0f22c4b1c643828d9b142e5d6258
ManuelNicanorWilliams <manu_williams@hotmail.com>
2025-10-24 15:55:00 -0300

    Add Ejercicio1Main class with main method

---
+++ TP7/ejercicio1/Ejercicio1Main.java
@@ -0,0 +1,8 @@
+package ejercicio1;
+
+public class Ejercicio1Main {
+    public static void main(String[] args) {
+        Auto miAuto = new Auto("Renault", "Clio", 4);
+        miAuto.mostrarInfo();
+    }
+}
